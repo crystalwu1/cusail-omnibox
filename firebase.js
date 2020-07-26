bd88f3815e560c7dc6c11494f297c353a559d34b
Crystal Wu <cw683@cornell.edu>
2020-07-26 12:24:58 -0400

    removed add and delete functionaility to account for new webpage

--- firebase.js
+++ firebase.js
@@ -9,58 +9,53 @@ var firebaseConfig = {
   appId: "1:782612915461:web:5df3976372f0736f2f436c",
   measurementId: "G-DLH92W1HM0"
 };
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-// firebase.analytics();
 
 console.log(firebase);
 var db = firebase.firestore();
 
+chrome.identity.getAuthToken({ 'interactive': true }, function (token) {
+  var credential = firebase.auth.GoogleAuthProvider.credential(null, token);
+  firebase.auth().signInWithCredential(credential);
+});
+
 chrome.omnibox.onInputEntered.addListener(
 
   function (text) {
-    if (text.slice(0, 3) == "add") {
-      var sliced = text.slice(4)
-      var space = sliced.search(" ")
-      var keyword = (sliced.slice(0, space))
-      var link = sliced.slice(space + 1)
-      db.collection("links").doc(keyword).set({
-        data: link
-      })
-        .then(function () {
-          console.log("Document successfully written!");
-        })
-        .catch(function (error) {
-          console.error("Error writing document: ", error);
-        });
-    }
-    else if (text.slice(0, 6) == "remove") {
-      var sliced = text.slice(7)
-      var keyword = (sliced.slice(0, sliced.length))
-      console.log("removed " + keyword)
-      db.collection("links").doc(keyword).delete().then(function () {
-        console.log("Document successfully deleted!");
-      }).catch(function (error) {
-        console.error("Error removing document: ", error);
-      });
-    }
-    else {
-      var keyword = db.collection("links").doc(text)
-      keyword.get().then(function (doc) {
-        if (doc.exists) {
-          console.log("Document data:", doc.data());
-          const parsed = (doc.data())
-          chrome.tabs.create({ url: parsed.data });
-          db.collection("links").doc(keyword).set({
-            data: keyword
-          })
-        } else {
-          console.log("No such document");
-        }
-      }).catch(function (error) {
-        console.log("Error getting document:", error);
-      })
-    }
-  }
-)
+    // if (text.slice(0, 3) == "add") {
+    //   var nameShortcutLink = text.slice(4)
+    //   var space = nameShortcutLink.search(" ")
+    //   var name = (nameShortcutLink.slice(0, space))
 
+    //   var shortcutLink = nameShortcutLink.slice(space + 1)
+    //   var space2 = nameShortcutLink.search(" ")
+    //   var shortcut = shortcutLink.slice(0, space2)
+    //   var link = shortcutLink.slice(space + 1)
+
+    //   db.collection('links').add({
+    //     name: name,
+    //     shortcut: shortcut,
+    //     data: link,
+    //     tags: ""
+    //   })
+    //     .then(function () {
+    //       console.log("Document successfully written!");
+    //       alert("successfully added")
+    //     })
+    //     .catch(function (error) {
+    //       console.error("Error writing document: ", error);
+    //     });
+    // }
+    // else {
+    db.collection("links").where("shortcut", "==", text).get().then(function (querySnapshot) {
+      querySnapshot.forEach(function (doc) {
+        console.log(doc.id, " => ", doc.data());
+        chrome.tabs.create({ url: doc.data().data });
+      });
+    })
+      .catch(function (error) {
+        console.log("Error getting documents: ", error);
+      });
+  }
+  // }
+)
